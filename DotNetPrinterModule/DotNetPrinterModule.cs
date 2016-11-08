5cb06da0882397d131afa5c652e69f0a83cdde3e
Jan Moriaux <janmoriaux@hotmail.com>
2016-11-08 09:21:03 +0100

    Added DotNetPrinterModule, DotNetSensorModule

---
+++ DotNetPrinterModule/DotNetPrinterModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.IoT.Gateway;
+
+namespace DotNetPrinterModule
+{
+    public class DotNetPrinterModule : IGatewayModule
+    {
+        private String configuration;
+
+        public void Create(Broker broker, byte[] configuration)
+        {
+            this.configuration = Encoding.UTF8.GetString(configuration);
+        }
+
+        public void Destroy()
+        {            
+        }
+
+        public void Receive(Message received_message)
+        {
+            if (received_message.Properties["source"] == "sensor")
+            {
+                Console.WriteLine("Printer Module received message from Sensor. Content " +
+                    Encoding.UTF8.GetString(received_message.Content, 0, 
+                    received_message.Content.Length));
+            }
+        }
+    }
+}
---
+++ DotNetSensorModule/DotNetSensorModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Azure.IoT.Gateway;
+
+namespace DotNetSensorModule
+{
+    public class DotNetSensorModule : IGatewayModule, IGatewayModuleStart
+    {
+        private Broker broker;
+        private String configuration;
+
+        public void Create(Broker broker, byte[] configuration)
+        {
+            this.broker = broker;
+            this.configuration = Encoding.UTF8.GetString(configuration);
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("This is C# Sensor Module Destroy!");
+        }
+
+        public void Receive(Message received_message)
+        {
+            //just ignore the Message, sensor doesn't need to print
+        }
+
+        public void Start()
+        {
+            Thread oThread = new Thread(new ThreadStart(this.threadBody));
+            oThread.Start();
+        }
+        public void threadBody()
+        {
+            Random r = new Random();
+            int n = r.Next();
+
+            while (true)
+            {
+                Dictionary<string, string> thisIsMyProperty = new Dictionary<string, string>();
+                thisIsMyProperty.Add("source", "sensor");
+
+                Message messageToPublish = new Message("SensorData: " + n, thisIsMyProperty);
+
+                this.broker.Publish(messageToPublish);
+
+                //Publish a message every 5 seconds.
+                Thread.Sleep(5000);
+                n = r.Next();
+            }
+        }
+    }
+}
