2f3186029bbb894d623230b3f3ce2dd30efd6a2f
Jan Moriaux <janmoriaux@hotmail.com>
2016-11-08 12:17:02 +0100

    DotNetLoggerModule writes start and end of log and writes received messages to filepath from json config

--- DotNetModules/DotNetLoggerModule.cs
+++ DotNetModules/DotNetLoggerModule.cs
@@ -20,14 +20,15 @@ namespace DotNetModules
     public class DotNetLoggerModule : IGatewayModule
     {
         private const String logDirectory = @"c:\temp\";
-        private String logFilePath; 
+        private String logFilePath;
         private String configuration;
 
         //configuration contains the filepath to log to
         public void Create(Broker broker, byte[] configuration)
         {
             this.configuration = Encoding.UTF8.GetString(configuration);
-            logFilePath= logDirectory + configuration;
+            logFilePath = logDirectory + this.configuration;
+            WriteToLog("Start of log");
         }
 
         public void Destroy()
@@ -37,24 +38,59 @@ namespace DotNetModules
 
         public void Receive(Message received_message)
         {
-            if (!Directory.Exists(logDirectory))
+            createLogDirectory();
+            try
             {
-                Directory.CreateDirectory(logDirectory);
-            }
-            if (!File.Exists(logFilePath))
-            {
-                File.Create(logFilePath);
-            }
-            using (StreamWriter writer = new StreamWriter(logDirectory + this.configuration, true))
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
             {
                 string content = Encoding.UTF8.GetString(received_message.Content, 0,
                     received_message.Content.Length);
                 string source = received_message.Properties["source"];
-                writer.WriteLine("Start");
+                writer.WriteLine("Start of message");
                 writer.WriteLine("Source: " + source);
                 writer.WriteLine("Content: " + content);
-                writer.WriteLine("Stop");
+                writer.WriteLine("End of message");
+                writer.WriteLine();
             }
-        }        
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Unable to write to file: " + logFilePath);
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private void WriteToLog(String text)
+        {
+            createLogDirectory();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                    writer.WriteLine(text);
+                    writer.WriteLine("Time: " + DateTime.UtcNow);
+                    writer.WriteLine();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Unable to write to file: " + logFilePath);
+                Console.WriteLine(e.Message);                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private void createLogDirectory()
+        {
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
     }
 }
