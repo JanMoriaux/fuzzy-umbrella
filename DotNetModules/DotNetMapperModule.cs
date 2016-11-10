49fe6f3ceffa404e6cd00789213e0abf0bd664a6
Jan Moriaux <janmoriaux@hotmail.com>
2016-11-10 08:48:48 +0100

    added correct messageproperties to communicate with native IoTHub module to DotNetMapperModule and DotNetSimulatedDevice

--- DotNetModules/DotNetMapperModule.cs
+++ DotNetModules/DotNetMapperModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Threading;
 using Microsoft.Azure.IoT.Gateway;
 using Newtonsoft.Json;
@@ -9,85 +10,155 @@ using System.Data;
 
 namespace DotNetModules
 {
-    //todo:    
-    /*load devices on module create
-     * mapper receives mac addresses from connected devices
-     * mapper receives deviceidentities from IoT Hub module
+
+    /* TODO:     
      * 
+     * mapper receives deviceidentities from IoT Hub module and automatically maps
+     * them to a MAC address
      */
-    public class DotNetMapperModule : IGatewayModule, IGatewayModuleStart
+    public class DotNetMapperModule : IGatewayModule //,IGatewayModuleStart
     {
         private Broker broker;
         private String configuration;
         private Dictionary<string, DeviceIdentity> devices;
+        //private Boolean gatewayIsReady = false;
 
-        public Dictionary<string, DeviceIdentity> Devices { get; }
 
         public void Create(Broker broker, byte[] configuration)
         {
             this.configuration = Encoding.UTF8.GetString(configuration);
             this.broker = broker;
-            Console.WriteLine("Mapper configuration: " + this.configuration);
-            //this.LoadDevices();
-            //foreach (var device in this.Devices)
-            //{
-            //    Console.WriteLine(device.Key + "\t" + device.Value.DeviceId + "\t" + device.Value.DeviceKey);
-            //}
-            //Console.WriteLine();
+            try
+            {
+                LoadDevices();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //Console.WriteLine("Mapper configuration: " + this.configuration);
         }
 
         public void Destroy()
         {
-            Console.WriteLine("OtherMapperModule.Destroy()!");
+            Console.WriteLine("DotNetMapperModule.Destroy()!");
         }
 
         public void Receive(Message received_message)
         {
+            //Mapper module receives message from device
+            //and automatically forwards it to the IoTHub module
+            //with device identity information
             if (received_message.Properties["source"] == "simdevice" &&
-                received_message.Properties["type"] == "macaddress")
+                received_message.Properties["type"] == "d2c")
+            {                
+                //look up the device's deviceKey and DeviceId
+                DeviceIdentity deviceIdentity = devices[received_message.Properties["macAddress"]];
+
+                //Create a message to publish to the broker, that will be received by the IoT-Hub module                                
+                Message msg = this.CreateMessage(deviceIdentity.DeviceId, deviceIdentity.DeviceKey,
+                    received_message.Content);
+
+                //publish to broker
+                this.Publish(msg);
+            }
+            //mapper receives message from IoTHub 
+            //automatically publishes message to broker
+            //aimed at a given device
+            if (received_message.Properties["source"] == "iothub" &&
+               received_message.Properties["type"] == "c2d")
             {
-                Console.WriteLine("Other mapper receives message from simdevice: " +
-                    Encoding.UTF8.GetString(received_message.Content), 0, received_message.Content.Length);
+                //look up the MAC-address
+                string deviceId = received_message.Properties["deviceName"];
+                string macAddress = devices.Keys.Where(p => devices[p].DeviceId == deviceId).
+                    Single().ToString();
+
+                //Create a message for the device
+                //with the given MAC-address
+                Message msg = CreateMessage(macAddress, null,
+                    received_message.Content);
+
+                //publish to broker
+                this.Publish(msg);
+            }
+        }
+        //public void Start()
+        //{
+        //    //Thread oThread = new Thread(new ThreadStart(ThreadBody));
+        //    //oThread.Start();
+        //    this.gatewayIsReady = true;
+        //}
+        private Message CreateMessage(string deviceName, string deviceKey, byte[] content)
+        {
+            Dictionary<string, string> msgProperties = new Dictionary<string, string>();
+            msgProperties["source"] = "mapping";
+            if (deviceKey != null)
+            {
+                msgProperties["deviceName"] = deviceName;
+                msgProperties["deviceKey"] = deviceKey;
+                msgProperties["type"] = "d2c";
+            }
+            else
+            {
+                msgProperties["macAddress"] = deviceName;
+                msgProperties["type"] = "c2d";
+            }
+            Message msg = new Message(content, msgProperties);
+            return msg;
+        }
+        private void Publish(Message message)
+        {
+            try
+            {
+                broker.Publish(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Mapper module: problem trying to publish to broker");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
         }
 
-        public void Start()
-        {
-            Thread oThread = new Thread(new ThreadStart(ThreadBody));
-            oThread.Start();
-        }
-        private void ThreadBody()
-        {
-            //try
-            //{
-            //    this.LoadDevices();
-            //    foreach (var device in this.Devices)
-            //    {
-            //        Console.WriteLine(device.Key + "\t" + device.Value.DeviceId + "\t" + device.Value.DeviceKey);
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
-            Dictionary<string, string> msgProperties = new Dictionary<string, string>();
-            msgProperties["source"] = "mappermodule";
-            Message msg = new Message("Mappings:\n" + this.configuration, msgProperties);
-            broker.Publish(msg);
-        }
-        //private void LoadDevices()
+        //private void ThreadBody()
         //{
-        //    this.devices = new Dictionary<string, DeviceIdentity>();
-        //    DataSet configSet = JsonConvert.DeserializeObject<DataSet>(this.configuration.Substring(1));
-        //    DataTable deviceTable = configSet.Tables["devices"];
-        //    foreach (DataRow row in deviceTable.Rows)
-        //    {
-        //        devices[row["macAddress"].ToString()] = new DeviceIdentity
-        //        {
-        //            DeviceKey = row["deviceKey"].ToString(),
-        //            DeviceId = row["deviceId"].ToString()
-        //        };
-        //    }
+        //    this.LoadDevices();
+        //    Dictionary<string, string> msgProperties = new Dictionary<string, string>();
+        //    msgProperties["source"] = "mapping";
+        //    Message msg = new Message("Mappings:\n" + this.configuration, msgProperties);
+        //    broker.Publish(msg);
         //}
+        private void LoadDevices()
+        {
+            try
+            {
+                //initializes a dictionary with key = macaddress and value = deviceidentity
+                //from the configuration
+                this.devices = new Dictionary<string, DeviceIdentity>();
+                DataSet configSet = JsonConvert.DeserializeObject<DataSet>(this.configuration);
+                DataTable deviceTable = configSet.Tables["devices"];
+                foreach (DataRow row in deviceTable.Rows)
+                {
+                    devices[row["macAddress"].ToString()] = new DeviceIdentity
+                    {
+                        DeviceKey = row["deviceKey"].ToString(),
+                        DeviceId = row["deviceId"].ToString()
+                    };
+                }
+
+                //prints the mapped devices to console
+                //Console.WriteLine("Devices:\n");
+                //foreach (var device in this.devices)
+                //{
+                //    Console.WriteLine(device.Key + "\t" + device.Value.DeviceId + "\t" + device.Value.DeviceKey);
+                //}
+                //Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
     }
 }
--- DotNetModules/DotNetSimulatedDevice.cs
+++ DotNetModules/DotNetSimulatedDevice.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.IoT.Gateway;
 using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace DotNetModules
 {
@@ -14,21 +16,23 @@ namespace DotNetModules
         private Broker broker;
         private String configuration;
 
+        //configuration used for mapping to an IoT hub device identity (DeviceId/DeviceKey)
+        //using real devices, the module would receive this (MAC)address from the device
         /// <summary>
         /// Invoked by the Module Host to create a new DotNetSimulatedDevice instance.
         /// </summary>
         /// <param name="broker">The Broker-instance that brokers messages to/from
         /// other modules in the gateway
         /// </param>        
-        /// <param name="configuration">Must a least contain a (physical) address for the 
-        /// device, used for mapping to an IoT hub device identity (DeviceId/DeviceKey).
-        /// Using real devices, the module would receive this (MAC)address from the device</param>
+        /// <param name="configuration">contains a physical address for the 
+        /// device
+        /// </param>
         public void Create(Broker broker, byte[] configuration)
         {
             this.configuration = Encoding.UTF8.GetString(configuration);
             this.broker = broker;
-            Console.WriteLine("Simulated device created: " + this.configuration);
-        }
+            //Console.WriteLine("Simulated device created: " + this.configuration);
+        }        
         /// <summary>
         /// Invoked by the Module Host to dispose of the DotNetSimulatedDevice instance.        
         /// </summary>
@@ -36,65 +40,112 @@ namespace DotNetModules
         {
             Console.WriteLine("This is DotNetSimulatedDevice.destroy()!");
         }
+        //The DotNetSimulatedDevice receives a message from the broker.
+        //A filter is applied to only accept c2d messages redirected
+        //through the mapper module.
+        //An acknowledgment message is sent back to the cloud.
         /// <summary>
-        /// The DotNetSimulatedDevice receives a message from the broker.
-        /// A filter is applied to only accept IoTHub-module messages
-        /// (for C2D-commands).
+        /// 
         /// </summary>
         /// <param name="received_message"></param>
         public void Receive(Message received_message)
         {
-            //if (received_message.Properties["source"] == "iothub")
-            //{
-            Console.WriteLine("DotNetSimulated device receives message from " +
-                received_message.Properties["source"] + ". Content: ");
-            Console.WriteLine(Encoding.UTF8.GetString(received_message.Content, 0,
-                received_message.Content.Length));
-            //}            
+            if (received_message.Properties["source"] == "mapping" &&
+                received_message.Properties["type"] == "c2d")
+            {                
+                //sends a message to the mapper that cloud info has been received                               
+                String msgContent = this.CreateJsonString("cloud message received");
+                Message ackMessage = new Message(msgContent, CreateMessageProperties());
+                this.Publish(ackMessage);                
+            }       
         }
         /// <summary>
-        /// Creates and starts a new thread that allows the DotNetSimulatedDevice 
-        /// to publish messages to the Broker, when the gateway is ready.
+        /// Creates and starts a new worker thread for publishing messages to the broker.
         /// </summary>
         public void Start()
         {
             Thread oThread = new Thread(new ThreadStart(this.ThreadBody));
             oThread.Start();
         }
-        /// <summary>
-        /// When gateway is ready: publishes its MAC address to the broker.
-        /// Every 5 seconds: publishes temperature information to the broker.
+        /// <summary>        
+        /// Publishes temperature info to the broker every 10 s.
         /// </summary>
         public void ThreadBody()
-        {
-            //Send a  message containing the MAC-address of the device
-            Dictionary<string, string> messageProperties = new Dictionary<string, string>();
-            messageProperties.Add("source", "simdevice");
-            messageProperties.Add("type", "macaddress");
-            Message macAddressMessage = new Message("MAC Address: " + this.configuration,
-                messageProperties);
-            this.broker.Publish(macAddressMessage);
-
+        {                        
+            //Send a message with temperature information every 10 seconds.
             double avgTemperature = 10.0;
             double addTemp = 0.0;
             double maxTemp = 40.0;
-
-            //Send a message with temperature information every 10 seconds.
             while (true)
             {
                 if (avgTemperature + addTemp > maxTemp)
                 {
                     addTemp = 0.0;
                 }
-                string tempString = (avgTemperature + addTemp).ToString();
-                messageProperties["type"] = "temperaturedata";
-                Message temperatureMessage = new Message("SimDevice Data: " + tempString + " Kelvin", 
-                    messageProperties);
-                this.broker.Publish(temperatureMessage);
+                string tempString = (avgTemperature + addTemp).ToString();                
+                String msgContent = this.CreateJsonString(tempString + "°C");
+                Message temperatureMessage = new Message(msgContent, 
+                    CreateMessageProperties());
+
+                this.Publish(temperatureMessage);
 
                 addTemp += 1.0;
-                
+
                 Thread.Sleep(10000);                
+            }
+        }
+        //set messageproperties according to the filter defined in 
+        //the Receive()-method of the mapper-module to send messages to cloud           
+        private Dictionary<string,string> CreateMessageProperties()
+        {
+            Dictionary<string, string> messageProperties = new Dictionary<string, string>();
+            messageProperties["source"] = "simdevice";
+            messageProperties["type"] = "d2c";
+            messageProperties["macAddress"] = this.configuration;
+            return messageProperties;
+        }        
+        private String CreateJsonString(string content)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+
+                writer.WriteStartObject();
+                writer.WritePropertyName("message");
+                writer.WriteStartArray();
+                writer.WriteStartObject();
+                writer.WritePropertyName("device");
+                writer.WriteValue(this.configuration);
+                writer.WriteEndObject();
+                writer.WriteStartObject();
+                writer.WritePropertyName("content");
+                writer.WriteValue(content);
+                writer.WriteEndObject();
+                writer.WriteEnd();
+                writer.WriteEndObject();
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Publishes messages to the broker
+        /// </summary>
+        /// <param name="msg">The Message instance to publish</param>
+        private void Publish(Message msg)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(msg.Content));
+            try
+            {
+                this.broker.Publish(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Device {0} encountered a problem publishing to the broker",
+                    this.configuration);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
